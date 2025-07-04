3c96d92994bfe2db90cb05e0866d72b078d919d9
Eslam Ashraf <66190335+Eslam-Ashraf-49@users.noreply.github.com>
2025-07-04 18:25:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,45 +5,43 @@ import face_recognition
 import os
 from datetime import datetime
 
-st.title(Face Recognition Attendance System)
+st.title("Face Recognition Attendance System")
 
 path = 'AttendanceImages'
 images = []
 classNames = []
 myList = os.listdir(path)
 
-for cl in myList
-    curImg = cv2.imread(f'{path}{cl}')
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
 
-def findEncodings(images)
+def findEncodings(images):
     encodeList = []
-    for img in images
+    for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
 
-def markAttendance(name)
-    with open('Attendance.csv', 'r+') as f
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
         myDataList = f.readlines()
         nameList = [line.split(',')[0] for line in myDataList]
-        if name not in nameList
+        if name not in nameList:
             now = datetime.now()
-            dtString = now.strftime('%H%M%S')
-            f.writelines(f'n{name},{dtString}')
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
 
 encodeListKnown = findEncodings(images)
-st.success(Encoding Complete)
+st.success("Encoding Complete")
 
 run = st.checkbox('Start Webcam')
-
 FRAME_WINDOW = st.image([])
-
 cap = cv2.VideoCapture(0)
 
-while run
+while run:
     success, img = cap.read()
     imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
@@ -51,16 +49,14 @@ while run
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame)
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
         matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]
+        if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            faceLoc = np.array(faceLoc)  0.25
-            faceLoc = faceLoc.astype(int)
-            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = [int(pos / 0.25) for pos in faceLoc]
 
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
